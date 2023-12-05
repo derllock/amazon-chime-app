f54683c7161ac9a7c634b1920bb2e8c239a3fac6
derllock <denissmaheshwari@gmail.com>
2023-12-06 02:12:22 +0530

    trying to repair client

---
+++ client/src/App.tsx
@@ -0,0 +1,284 @@
+import {
+  AuthBindings,
+  Authenticated,
+  Refine,
+} from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  notificationProvider,
+  RefineSnackbarProvider,
+  ThemedLayoutV2,
+} from "@refinedev/mui";
+
+import { MuiInferencer } from "@refinedev/inferencer/mui";  //i made this
+
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import axios, { AxiosRequestConfig } from "axios";
+import { CredentialResponse } from "interfaces/google";
+import {
+  BlogPostCreate,
+  BlogPostEdit,
+  BlogPostList,
+  BlogPostShow,
+} from "pages/blog-posts";
+import {
+  CategoryCreate,
+  CategoryEdit,
+  CategoryList,
+  CategoryShow,
+} from "pages/categories";
+import { Login } from "pages/login";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { parseJwt } from "utils/parse-jwt";
+import { Header } from "./components/header";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
+  const token = localStorage.getItem("token");
+  if (request.headers) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  } else {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return request;
+});
+
+function App() {
+  const authProvider: AuthBindings = {
+    login: async ({ credential }: CredentialResponse) => {
+      const profileObj = credential ? parseJwt(credential) : null;
+
+      if (profileObj) {
+        //save user to mongodb
+        const response=await fetch('http://localhost:8080/api/v1/users',{
+          method:'POST',
+          headers:{'Content-Type':'application/json'},
+          body:JSON.stringify({
+            name:profileObj.name,
+            email:profileObj.email,
+            avatar:profileObj.picture,
+          })
+        })
+        const data=await response.json();
+
+        if(response.status===200)
+          {
+            localStorage.setItem(
+            "user",
+            JSON.stringify({
+              ...profileObj,
+              avatar: profileObj.picture,
+              userid:data._id,
+            })
+          );
+
+          localStorage.setItem("token", `${credential}`);
+
+          return {
+            success: true,
+            redirectTo: "/",
+          };
+        }
+        else{
+          return {
+            success: false,
+          };
+        }
+      }
+
+      return {
+        success: false,
+      };
+    },
+    logout: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token && typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        axios.defaults.headers.common = {};
+        window.google?.accounts.id.revoke(token, () => {
+          return {};
+        });
+      }
+
+      return {
+        success: true,
+        redirectTo: "/login",
+      };
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return {
+          authenticated: true,
+        };
+      }
+
+      return {
+        authenticated: false,
+        error: {
+          message: "Check failed",
+          name: "Token not found",
+        },
+        logout: true,
+        redirectTo: "/login",
+      };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        return JSON.parse(user);
+      }
+
+      return null;
+    },
+  };
+
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <CssBaseline />
+          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+          <RefineSnackbarProvider>
+            <Refine
+              dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              notificationProvider={notificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              resources={[
+                {
+                  name: "teams",
+                  list: MuiInferencer,
+                  meta: {
+                      label: "Teams",
+                      icon: "",
+                  },
+              },{
+                name: "contacts",
+                list: MuiInferencer,
+                meta: {
+                    label: "Contacts",
+                    icon: "",
+                },
+            },{
+              name: "calendar",
+              list: MuiInferencer,
+              meta: {
+                  label: "Calendar",
+                  icon: "",
+              },
+          },{
+            name: "meetings",
+            list: MuiInferencer,
+            meta: {
+                label: "Meetings",
+                icon: "",
+            },
+        },{
+                  name: "dashboard",
+                  list: MuiInferencer,
+                  meta: {
+                      label: "Dashboard",
+                      icon: "🏠",
+                  },
+              },
+                // {
+                //   name: "blog_posts",
+                //   list: "/blog-posts",
+                //   create: "/blog-posts/create",
+                //   edit: "/blog-posts/edit/:id",
+                //   show: "/blog-posts/show/:id",
+                //   meta: {
+                //     canDelete: true,
+                //   },
+                // },
+                // {
+                //   name: "categories",
+                //   list: "/categories",
+                //   create: "/categories/create",
+                //   edit: "/categories/edit/:id",
+                //   show: "/categories/show/:id",
+                //   meta: {
+                //     canDelete: true,
+                //   },
+                // },
+              ]}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "YJbmCo-8S2hGK-8MPl1g",
+              }}
+            >
+              <Routes>
+                <Route
+                  element={
+                    <Authenticated fallback={<CatchAllNavigate to="/login" />}>
+                      <ThemedLayoutV2 Header={() => <Header isSticky={true} />}>
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                >
+                  <Route
+                    index
+                    element={<NavigateToResource resource="blog_posts" />}
+                  />
+                  <Route path="/blog-posts">
+                    <Route index element={<BlogPostList />} />
+                    <Route path="create" element={<BlogPostCreate />} />
+                    <Route path="edit/:id" element={<BlogPostEdit />} />
+                    <Route path="show/:id" element={<BlogPostShow />} />
+                  </Route>
+                  <Route path="/categories">
+                    <Route index element={<CategoryList />} />
+                    <Route path="create" element={<CategoryCreate />} />
+                    <Route path="edit/:id" element={<CategoryEdit />} />
+                    <Route path="show/:id" element={<CategoryShow />} />
+                  </Route>
+                  <Route path="*" element={<ErrorComponent />} />
+                </Route>
+                <Route
+                  element={
+                    <Authenticated fallback={<Outlet />}>
+                      <NavigateToResource />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<Login />} />
+                </Route>
+              </Routes>
+
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+          </RefineSnackbarProvider>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
